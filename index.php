9b1d202000ba316c7daa57c428b009c9176e6fef
anggasuta92 <angga.suta92@gmail.com>
2018-04-12 14:21:31 +0800

    index

--- index.php
+++ index.php
@@ -3,14 +3,29 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title>iBook rSoft &copy;2017</title>
         <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-
+		<title>Bank</title>
+		
         <link rel="stylesheet" href="asset/css/bootstrap.min.css" rel="stylesheet"  type="text/css">
+		<link rel="stylesheet" href="asset/general.css" rel="stylesheet"  type="text/css">
         <script src="asset/jquery-2.2.3.min.js"></script>
         <script src="asset/js/bootstrap.min.js"></script>
     </head>
 
     <body>
+	
+		<div class="container">
+			<div class="page-header">
+			  <h1>This is a header <small>Subtext for header</small></h1>
+			</div>	
+			<ul class="nav nav-tabs">
+			  <li class="active"><a href="#">Home</a></li>
+			  <li><a href="#">Menu 1</a></li>
+			  <li><a href="#">Menu 2</a></li>
+			  <li><a href="#">Menu 3</a></li>
+			</ul>	
+
+		</div>
+		
     </body>
 </html>
