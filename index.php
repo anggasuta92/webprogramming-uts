410cd9e66139a8002e9a0c12bc691b00008d8ca7
anggasuta92 <angga.suta92@gmail.com>
2018-04-12 12:59:58 +0800

    jquery

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>iBook rSoft &copy;2017</title>
+        <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+
+        <link rel="stylesheet" href="asset/css/bootstrap.min.css" rel="stylesheet"  type="text/css">
+        <script src="asset/jquery-2.2.3.min.js"></script>
+        <script src="asset/js/bootstrap.min.js"></script>
+    </head>
+
+    <body>
+    </body>
+</html>
